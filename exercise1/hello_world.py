27dfdfc0d35e47e377503ee7dde3ce673758e09f
hdantas <hndantas@gmail.com>
2016-10-17 23:41:53 +0100

    Added requirements and gitignore. Created exercise1.

---
+++ exercise1/hello_world.py
@@ -0,0 +1,21 @@
+from sulley import *
+
+s_initialize('hello_world')
+
+s_string('GET')
+s_delim(' ')
+s_string('/')
+s_delim(' ')
+s_static(' HTTP/1.1\r\n')
+s_static('Host: 0.0.0.0:5000\r\n')
+s_static('\r\n')
+
+
+# GET / HTTP/1.1
+# Host: localhost:5000
+
+sess = sessions.session()
+target = sessions.target("localhost", 5000)
+sess.add_target(target)
+sess.connect(s_get("hello_world"))
+sess.fuzz()
