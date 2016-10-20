a0e2d5d00a06a90ee186c7b7ead621a344bfddf3
hdantas <hndantas@gmail.com>
2016-10-20 09:32:13 +0100

    Added example request.

--- exercise2/vuln_api.py
+++ exercise2/vuln_api.py
@@ -4,3 +4,12 @@ from sulley import (
     s_block_start, s_block_end,
     sessions
 )
+
+# POST /api/pet HTTP/1.1
+# Host: vulnerableapi.herokuapp.com
+# Accept: */*
+# Content-Type: application/json
+# x-access-token: supersecrettoken
+# Content-Length: 54
+#
+# {"id":1,"name":"Whiskers","breed":"Cat","tag":"White"}
